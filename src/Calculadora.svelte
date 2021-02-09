3ffb9391906401989c99eed45aa7e5b55fa9d2e9
6rak0 <ego.6rak0@gmail.com>
2021-02-08 19:18:03 -0600

    beta

--- src/Calculadora.svelte
+++ src/Calculadora.svelte
@@ -1,74 +1,74 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
+  import { createEventDispatcher } from 'svelte'
 
-    const dispatch = createEventDispatcher();
-    let tot = null;
-    let lab = null;
-    let mat = null;
-    let hon = null;
-    let isr = null;
+  const dispatch = createEventDispatcher()
+  let tot = null
+  let lab = null
+  let mat = null
+  let hon = null
+  let isr = null
 
-    function calcular() {
-        const efectivo = (tot - lab - mat - hon - isr).toFixed(2);
-        const terminal = (tot - tot * 0.0336 - lab - mat - hon - isr).toFixed(2);
-        const tresMeses = (tot - tot * 0.087 - lab - mat - hon - isr).toFixed(2);
-        const seisMeses = (tot - tot * 0.1218 - lab - mat - hon - isr).toFixed(2);
-        const nueveMeses = (tot - tot * 0.145 - lab - mat - hon - isr).toFixed(2);
-        const doceMeses = (tot - tot * 0.1798 - lab - mat - hon - isr).toFixed(2);
-        const totales = {
-            efectivo,
-            terminal,
-            tresMeses,
-            seisMeses,
-            nueveMeses,
-            doceMeses
-        }
-        dispatch('calculos', totales);
+  function calcular() {
+    const efectivo = (tot - lab - mat - hon - isr).toFixed(2)
+    const izettle = (tot - tot * 0.0406 - lab - mat - hon - isr).toFixed(2)
+    const tresMeses = (tot - tot * 0.0814 - lab - mat - hon - isr).toFixed(2)
+    const seisMeses = (tot - tot * 0.1162 - lab - mat - hon - isr).toFixed(2)
+    const nueveMeses = (tot - tot * 0.151 - lab - mat - hon - isr).toFixed(2)
+    const doceMeses = (tot - tot * 0.1914 - lab - mat - hon - isr).toFixed(2)
+    const totales = {
+      efectivo,
+      izettle,
+      tresMeses,
+      seisMeses,
+      nueveMeses,
+      doceMeses,
     }
+    dispatch('calculos', totales)
+  }
 </script>
 
-<style>
-    form{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    h2{
-        margin: 2px;
-        padding: 2px;
-    }
-    input {
-        background-color: var(--background);
-        border: none;
-        border-bottom: 2px solid var(--button);
-        font-size: 1.5rem;
-        border-radius: 0;
-        color: var(--paragraph);
-    }
-    button{
-        background-color: var(--button);
-        border-radius: 5px;
-        padding: 10px 20px;
-        margin: 10px;
-        color: var(--button-text);
-        font-size: 1.5rem;
-    }
-    label{
-        width: 70%;
-        text-align: right;
-    }
-</style>
-
 <form on:submit|preventDefault={calcular}>
-    <label>total</label>
-    <h2>$<input type="number" bind:value={tot}></h2>
-    <label>laboratorio</label>
-    <h2>$<input type="number" bind:value={lab}></h2>
-    <label>material</label>
-    <h2>$<input type="number" bind:value={mat}></h2>
-    <label>honorarios</label>
-    <h2>$<input type="number" bind:value={hon}></h2>
-    <label>isr</label>
-    <h2>$<input type="number" bind:value={isr}></h2>
-    <button>calcular</button>
+  <label>total</label>
+  <h2>$<input type="number" bind:value={tot} /></h2>
+  <label>laboratorio</label>
+  <h2>$<input type="number" bind:value={lab} /></h2>
+  <label>material</label>
+  <h2>$<input type="number" bind:value={mat} /></h2>
+  <label>honorarios</label>
+  <h2>$<input type="number" bind:value={hon} /></h2>
+  <label>isr</label>
+  <h2>$<input type="number" bind:value={isr} /></h2>
+  <button>calcular</button>
 </form>
+
+<style>
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  h2 {
+    margin: 2px;
+    padding: 2px;
+  }
+  input {
+    background-color: var(--background);
+    border: none;
+    border-bottom: 2px solid var(--button);
+    font-size: 1.5rem;
+    border-radius: 0;
+    color: var(--paragraph);
+  }
+  button {
+    background-color: var(--button);
+    border-radius: 5px;
+    padding: 10px 20px;
+    margin: 10px;
+    color: var(--button-text);
+    font-size: 1.5rem;
+  }
+  label {
+    width: 70%;
+    text-align: right;
+  }
+</style>
--- src/Display.svelte
+++ src/Display.svelte
@@ -1,62 +1,66 @@
 <script>
-    import Calculadora from './Calculadora.svelte'
+  import Calculadora from './Calculadora.svelte'
 
-    let data = [];
+  let data = []
 
-    function desplegarResultados(e){
-        const totales = e.detail;
-        data = [totales];        
-	}
+  function desplegarResultados(e) {
+    const totales = e.detail
+    data = [totales]
+  }
 </script>
 
-<style>
-    p{
-        text-align: center;
-        font-size: 20px;
-    }
-    div{
-        max-width: 80%;
-        margin: 10px auto;
-    }
-    table{
-        font-size: 1.3em;
-        color: var(--headline);
-        padding-top: 20px;
-        width: 100%;
-        text-align: center;
-    }
-</style>
-
-<Calculadora on:calculos={desplegarResultados}/>
+<Calculadora on:calculos={desplegarResultados} />
 {#each data as totales}
-<div>
+  <div>
     <table>
-        <tr>
-            <td>efectivo</td>
-            <td>${totales.efectivo}</td>
-        </tr>
-        <tr>
-            <td>terminal</td>
-            <td>${totales.terminal}</td>
-        </tr>
-        <tr>
-            <td>3 meses</td>
-            <td>${totales.tresMeses}</td>
-        </tr>
-        <tr>
-            <td>6 meses</td>
-            <td>${totales.seisMeses}</td>
-        </tr>
-        <tr>
-            <td>9 meses</td>
-            <td>${totales.nueveMeses}</td>
-        </tr>
-        <tr>
-            <td>12 meses</td>
-            <td>${totales.doceMeses}</td>
-        </tr>
+      <tr>
+        <td>efectivo</td>
+        <td>${totales.efectivo}</td>
+      </tr>
+      <tr>
+        <td>terminal</td>
+        <td>${totales.izettle}</td>
+      </tr>
+      <tr>
+        <td>comisión</td>
+        <td>${(totales.efectivo - totales.izettle).toFixed(2)}</td>
+      </tr>
+      <tr>
+        <td>3 meses</td>
+        <td>${totales.tresMeses}</td>
+      </tr>
+      <tr>
+        <td>6 meses</td>
+        <td>${totales.seisMeses}</td>
+      </tr>
+      <tr>
+        <td>9 meses</td>
+        <td>${totales.nueveMeses}</td>
+      </tr>
+      <tr>
+        <td>12 meses</td>
+        <td>${totales.doceMeses}</td>
+      </tr>
     </table>
-</div>
+  </div>
 {:else}
-    <p>ingresa los datos</p>
+  <p>ingresa los datos</p>
 {/each}
+
+<style>
+  p {
+    text-align: center;
+    font-size: 20px;
+  }
+  div {
+    max-width: 80%;
+    margin: 10px auto;
+  }
+  table {
+    font-size: 1.3em;
+    color: var(--headline);
+    padding-top: 20px;
+    width: 100%;
+    text-align: center;
+  }
+</style>
